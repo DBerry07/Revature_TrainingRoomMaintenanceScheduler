8a415e5955eca8ae58dad3e0399e4b41ee82f360
novamapp <ogheneovoabu@gmail.com>
2020-02-14 15:16:41 -0500

    Created global RecyclerViewAdapter for simple strings

--- app/src/main/java/com/revature/roommaintenanceprototype/fragment/SM_Schedule_CampusSelectionFragment.java
+++ app/src/main/java/com/revature/roommaintenanceprototype/fragment/SM_Schedule_CampusSelectionFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,26 +17,22 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.revature.roommaintenanceprototype.R;
+import com.revature.roommaintenanceprototype.adapter.SimpleStringAdapter;
 import com.revature.roommaintenanceprototype.helper.FragmentHelper;
 import com.revature.roommaintenanceprototype.util.DummyText;
 import com.revature.roommaintenanceprototype.util.FragmentStringTags;
 import com.revature.roommaintenanceprototype.util.ScreenMessage;
 
+import java.util.ArrayList;
+
 public class SM_Schedule_CampusSelectionFragment extends Fragment implements View.OnClickListener{
-    Spinner spnCampus;
     Button btnCampusSelection;
+    RecyclerView rvCampuses;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_campus_selection, container, false);
-
-        spnCampus = (Spinner) rootView.findViewById(R.id.spn_campusSelection);
-        if(spnCampus != null){
-            spnCampus.setAdapter( new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_spinner_item, DummyText.getCampuses()) );
-        }else{
-            ScreenMessage.toastLongMsg(rootView.getContext(),"Error loading campuses");
-        }
 
         btnCampusSelection = (Button)rootView.findViewById(R.id.btn_campusSelection);
         if(btnCampusSelection != null){
@@ -41,6 +40,11 @@ public class SM_Schedule_CampusSelectionFragment extends Fragment implements Vie
         }else{
             ScreenMessage.toastLongMsg(rootView.getContext(),"Error loading button action for campus selection.");
         }
+
+        rvCampuses = (RecyclerView) rootView.findViewById(R.id.rv_campusSelection);
+        rvCampuses.setLayoutManager(new LinearLayoutManager(getActivity()));
+        Log.d("TESTING", ""+(new SimpleStringAdapter( (ArrayList<String>) DummyText.getCampuses())).getItemCount() );
+        rvCampuses.setAdapter(new SimpleStringAdapter( (ArrayList<String>) DummyText.getCampuses()));
 
         return rootView;
     }
