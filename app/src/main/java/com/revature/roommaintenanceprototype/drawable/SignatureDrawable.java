fa58d588006e200196d78adaa6a222ca58d08a54
novamapp <ogheneovoabu@gmail.com>
2020-02-20 13:09:14 -0500

    Got canvas to show. Still need to work on drawing path.

--- app/src/main/java/com/revature/roommaintenanceprototype/drawable/SignatureDrawable.java
+++ app/src/main/java/com/revature/roommaintenanceprototype/drawable/SignatureDrawable.java
@@ -1,4 +1,92 @@
 package com.revature.roommaintenanceprototype.drawable;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
 public class SignatureDrawable {
+    private Canvas canvas;
+    private Paint paint;
+    private Bitmap bitmap;
+    private int signatureHeight, signatureWidth;
+    private ImageView imageView;
+    private ArrayList<Point> pathPoints = new ArrayList<>();
+    private Path pathGraphic;
+
+    private SignatureDrawable(){}
+
+    public SignatureDrawable(int signatureWidth, int signatureHeight){
+        this.signatureWidth = signatureWidth;
+        this.signatureHeight = signatureHeight;
+        Log.d("DIMEN",signatureWidth+" | "+signatureHeight);
+        initPaint();
+        initBitmap();
+        initCanvas();
+        pathGraphic = new Path();
+        fillPathWithDummyPoints();
+    }
+
+    private void fillPathWithDummyPoints(){
+        for(int i=0; i<signatureWidth;i++){
+            pathPoints.add(new Point(0,i));
+        }
+    }
+
+    private void initPaint(){
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(3);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    private void initCanvas(){
+        if(bitmap != null){
+            canvas = new Canvas(bitmap);
+            canvas.drawColor(Color.YELLOW);//set background color of canvas
+        }
+    }
+
+    private void initBitmap(){
+        bitmap = Bitmap.createBitmap(
+             signatureWidth,
+             signatureHeight,
+             Bitmap.Config.ARGB_8888
+        );
+    }
+
+    public Bitmap drawPath(){
+        //canvas.drawPath(pathGraphic, paint);
+        Paint paint = new Paint();
+        Path path = new Path();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.TRANSPARENT);
+        canvas.drawPaint(paint);
+        for (int i = 50; i < 100; i++) {
+            path.moveTo(i, i-1);
+            path.lineTo(i, i);
+        }
+        path.close();
+        paint.setStrokeWidth(3);
+        paint.setPathEffect(null);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, paint);
+        return bitmap;
+    }
+
+    class Point{
+        float x,y;
+
+        private Point(){}
+        Point(float x, float y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
--- app/src/main/java/com/revature/roommaintenanceprototype/fragments/TR_Verify_SignatureFragment.java
+++ app/src/main/java/com/revature/roommaintenanceprototype/fragments/TR_Verify_SignatureFragment.java
@@ -6,34 +6,56 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.revature.roommaintenanceprototype.R;
 import com.revature.roommaintenanceprototype.controllers.workflowpersistance.TRVerifyPersistance;
+import com.revature.roommaintenanceprototype.drawable.SignatureDrawable;
 import com.revature.roommaintenanceprototype.util.fragmenthelpers.FragmentHelper;
 import com.revature.roommaintenanceprototype.util.ScreenMessage;
 
 public class TR_Verify_SignatureFragment extends Fragment {
     private static final String DEBUG_TAG = "TR_Verify_SignatureFragment";
+    private ImageView imgSignature;
+    private SignatureDrawable signatureDrawable;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_container, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_container, container, false);
         FragmentHelper.includeFragmentContent(R.layout.fragment_signature, (ViewGroup) rootView,inflater);
+        rootView.post(new Runnable() {
+            @Override
+            public void run() {
+                init(rootView);
+                drawSignature(imgSignature);
+            }
+        });
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        init(view);
         super.onViewCreated(view, savedInstanceState);
     }
 
+
+
     private void init(View view){
         FragmentHelper.updateToolbarTitle( (AppCompatActivity) getActivity(), "TR_Verify | "+getString(R.string.title_signature) );
+        imgSignature = view.findViewById(R.id.img_Signature);
     }
 
+    private void drawSignature(ImageView imageView){
+        //Log.d("DIMEN",""+imageView.getRootView());
+        int viewHeight = imageView.getHeight();
+        int viewWidth = imageView.getWidth();
+        Log.d("DIMEN","Before call construct");
+        signatureDrawable = new SignatureDrawable(viewWidth,viewHeight);
+        imageView.setImageBitmap(signatureDrawable.drawPath());
+    }
 }
