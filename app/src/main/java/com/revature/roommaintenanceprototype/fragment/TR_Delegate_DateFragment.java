41e69633fa7a6d9d430127edd6775f40bc481924
novamapp <55009153+novamapp@users.noreply.github.com>
2020-02-19 20:57:14 -0600

    Modularity completed in Criteria and Date fragments

--- app/src/main/java/com/revature/roommaintenanceprototype/fragment/TR_Delegate_DateFragment.java
+++ app/src/main/java/com/revature/roommaintenanceprototype/fragment/TR_Delegate_DateFragment.java
@@ -5,74 +5,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.revature.roommaintenanceprototype.R;
-import com.revature.roommaintenanceprototype.adapter.OnSetDateListener;
+import com.revature.roommaintenanceprototype.util.fragmenthelpers.DelegateDateHelper;
 import com.revature.roommaintenanceprototype.util.fragmenthelpers.FragmentHelper;
-import com.revature.roommaintenanceprototype.util.InputProcessing;
-import com.revature.roommaintenanceprototype.util.ScreenMessage;
 
 public class TR_Delegate_DateFragment extends Fragment implements View.OnClickListener{
-    ImageView iconStartDate, iconEndDate;
-    EditText etStartDate, etEndDate;
+    private ImageView iconStartDate, iconEndDate;
+    private EditText etStartDate, etEndDate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Toast.makeText(getContext(), "StartDate", Toast.LENGTH_LONG);
-        final View rootView =
-                inflater.inflate(R.layout.fragment_delegate_date, container, false);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_delegate_date, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        FragmentHelper.updateToolbarTitle( (AppCompatActivity) getActivity(), "TR_Delegate | "+getString(R.string.title_date_selection) );
-
-        iconStartDate = (ImageView)view.findViewById(R.id.img_startDate_icon);
-        iconStartDate.setOnClickListener(this);
-        iconEndDate = (ImageView)view.findViewById(R.id.img_endDate_icon);
-        iconEndDate.setOnClickListener(this);
-
-        etStartDate = (EditText)view.findViewById(R.id.et_startDate);
-        etEndDate = (EditText)view.findViewById(R.id.et_endDate);
-
-        String results = "";
-        String chosenTrainer = getArguments().getString( getString(R.string.argument_tr_delegate_selected_trainer) );
-        results+=" TRAINER: "+chosenTrainer;
-        String chosenRoom = getArguments().getString( getString(R.string.argument_tr_delegate_selected_room) );
-        results+=" ROOM: "+chosenRoom;
-        ScreenMessage.toastShortMsg(getContext(),results);
-
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        init(view);
         super.onViewCreated(view, savedInstanceState);
     }
 
+    public void init(View view) {
+        FragmentHelper.updateToolbarTitle( (AppCompatActivity) getActivity(), "TR_Delegate | "+getString(R.string.title_date_selection) );
+        iconStartDate = (ImageView) view.findViewById(R.id.img_startDate_icon);
+        iconStartDate.setOnClickListener(this);
+        iconEndDate = (ImageView) view.findViewById(R.id.img_endDate_icon);
+        iconEndDate.setOnClickListener(this);
+        etStartDate = (EditText) view.findViewById(R.id.et_startDate);
+        etEndDate = (EditText) view.findViewById(R.id.et_endDate);
+    }
+
     @Override
-    public void onClick(View view){
-        switch(view.getId()){
-            case R.id.img_startDate_icon:DialogFragment startDatePicker = new DatePickerFragment(new OnSetDateListener() {
-                    @Override
-                    public void onSetDate(DatePicker view, int year, int month, int day) {
-                        etStartDate.setText(InputProcessing.formatDate(year,month,day));
-                    }
-                });
-                startDatePicker.show(getActivity().getSupportFragmentManager(),"StartDate");
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_startDate_icon:
+                DelegateDateHelper.openStartDatePicker(etStartDate,getActivity().getSupportFragmentManager());
                 break;
-            case R.id.img_endDate_icon:DialogFragment endDatePicker = new DatePickerFragment(new OnSetDateListener() {
-                    @Override
-                    public void onSetDate(DatePicker view, int year, int month, int day) {
-                        etEndDate.setText(InputProcessing.formatDate(year,month,day));
-                    }
-                });
-                endDatePicker.show(getActivity().getSupportFragmentManager(),"EndDate");
+            case R.id.img_endDate_icon:
+                DelegateDateHelper.openStartDatePicker(etEndDate,getActivity().getSupportFragmentManager());
                 break;
         }
     }
--- app/src/main/java/com/revature/roommaintenanceprototype/fragment/TR_Verify_CriteriaSelectionFragment.java
+++ app/src/main/java/com/revature/roommaintenanceprototype/fragment/TR_Verify_CriteriaSelectionFragment.java
@@ -19,6 +19,7 @@ import com.revature.roommaintenanceprototype.R;
 import com.revature.roommaintenanceprototype.adapter.CriteriaAdapter;
 import com.revature.roommaintenanceprototype.adapter.OnChangeSwitchState;
 import com.revature.roommaintenanceprototype.adapter.OnItemClickListener;
+import com.revature.roommaintenanceprototype.util.fragmenthelpers.CriteriaSelectionHelper;
 import com.revature.roommaintenanceprototype.util.fragmenthelpers.FragmentHelper;
 import com.revature.roommaintenanceprototype.util.DummyText;
 import com.revature.roommaintenanceprototype.util.ScreenMessage;
@@ -29,6 +30,7 @@ import java.util.Set;
 
 
 public class TR_Verify_CriteriaSelectionFragment extends Fragment implements View.OnClickListener, OnItemClickListener, OnChangeSwitchState {
+    RecyclerView rvCleaningCriteria;
     NavController navController;
     Button button;
     Set<String> choosenCriteria = new HashSet<>();
@@ -37,66 +39,45 @@ public class TR_Verify_CriteriaSelectionFragment extends Fragment implements Vie
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_criteria_selection, container, false);
-
-        RecyclerView rvCleaningCriteria = FragmentHelper.initRecyclerView(rootView,R.id.rv_cleaningCriteria, getActivity(),
-                new CriteriaAdapter( (ArrayList<String>) DummyText.getCleaningCriteria(),this,this ));
-        button = (Button)rootView.findViewById(R.id.btn_criteriaSelection);
-        button.setOnClickListener(this);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_criteria_selection, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
-        FragmentHelper.updateToolbarTitle( (AppCompatActivity) getActivity(), "TR_Verify | "+getString(R.string.title_cleaningCriteria_selection) );
-
-        navController = Navigation.findNavController(view);
-        bundle = new Bundle();
+        init(view);
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void init(View view){
+        rvCleaningCriteria = FragmentHelper.initRecyclerView(view,R.id.rv_cleaningCriteria, getActivity(),
+                new CriteriaAdapter( (ArrayList<String>) DummyText.getCleaningCriteria(),this,this ));
+        button = (Button)view.findViewById(R.id.btn_criteriaSelection);
+        button.setOnClickListener(this);
+        FragmentHelper.updateToolbarTitle( (AppCompatActivity) getActivity(), "TR_Verify | "+getString(R.string.title_cleaningCriteria_selection) );
+        navController = Navigation.findNavController(view);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_criteriaSelection:
-                String[] choosenCriteriaArray = new String[choosenCriteria.size()];
-                choosenCriteria.toArray(choosenCriteriaArray);
-                ScreenMessage.toastShortMsg(getContext(), getString(R.string.argument_tr_verify_selected_criteria));
-                //bundle.putStringArray(getString(R.string.argument_tr_verify_selected_criteria).toString(),choosenCriteriaArray);
-                bundle.putString(getString(R.string.argument_tr_verify_selected_room), getArguments().getString( getString(R.string.argument_tr_verify_selected_room) ));
-
-                navController.navigate(R.id.action_TR_Verify_CriteriaSelectionFragment2_to_TR_Verify_SignatureFragment2,bundle);
+                FragmentHelper.navigateBetweenFragments(navController,
+                        null,
+                        R.id.action_TR_Verify_CriteriaSelectionFragment2_to_TR_Verify_SignatureFragment2);
                 break;
         }
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        TextView tvTitle = view.findViewById(R.id.tv_cleaningCriteria_item);
-        String title = tvTitle.getText().toString();
-
         Switch swt = view.findViewById(R.id.swt_cleaningCriteria_item);
-        boolean isChecked;
-
-        if(swt != null){
-            swt.setChecked(!swt.isChecked());
-            isChecked = swt.isChecked();
-            updateChosenCriteria(title, isChecked);
-        }else{
-            ScreenMessage.toastShortMsg(getContext(), "Switch is null");
-        }
+        TextView tvTitle = view.findViewById(R.id.tv_cleaningCriteria_item);
+        CriteriaSelectionHelper.updateSwitchState(swt, getContext());
+        CriteriaSelectionHelper.updateChosenCriteriaList(choosenCriteria,tvTitle.getText().toString(), swt.isChecked());
     }
 
     @Override
     public void onSwitchChanged(String title,View view, boolean isChecked){
-        updateChosenCriteria(title, isChecked);
-    }
-
-    private void updateChosenCriteria(String title, boolean isChecked){
-        if( isChecked){
-            choosenCriteria.add(title);
-        }else{
-            choosenCriteria.remove(title);
-        }
+        CriteriaSelectionHelper.updateChosenCriteriaList(choosenCriteria,title, isChecked);
     }
 }
